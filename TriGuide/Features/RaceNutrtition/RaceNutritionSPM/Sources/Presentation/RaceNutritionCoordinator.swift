0356289a6144bbdf89f480dcc0438594fada3ff7
eliasasskali <89183611+eliasasskali@users.noreply.github.com>
2026-01-26 21:27:12 +0100

    test periphery

--- TriGuide/Features/RaceNutrtition/RaceNutritionSPM/Sources/Presentation/RaceNutritionCoordinator.swift
+++ TriGuide/Features/RaceNutrtition/RaceNutritionSPM/Sources/Presentation/RaceNutritionCoordinator.swift
@@ -29,7 +29,7 @@ public class RaceNutritionCoordinator: BaseCoordinator<RaceNutritionCoordinator.
     @Published public var viewModel: RaceNutritionViewModel
     private let factory: RaceNutritionViewFactory
     private var carbItemsCoordinator: CarbItemsCoordinator?
-    var nonUsedProperty: Bool = false
+    var unusedProp: Bool = false
 
     // MARK: - Initializer
 
